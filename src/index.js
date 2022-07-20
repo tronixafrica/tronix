c8695274b42600f478d20b4c7f4bef2fbc616e8b
chukusuccess <successchuku5@gmail.com>
2022-07-20 16:56:08 +0100

    mobile dashboard done

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router } from 'react-router-dom'
-import 'tw-elements'
-import HeaderContextProvider from './state/contexts/HeaderContext';
-import DisplaySidebarProvider from './state/contexts/DisplaySidebarContext';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter as Router } from "react-router-dom";
+import "tw-elements";
+import HeaderContextProvider from "./state/contexts/HeaderContext";
+import DisplaySidebarProvider from "./state/contexts/DisplaySidebarContext";
+import "swiper/css/bundle";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router>

92d5977725423807a197962da00097766f1124bf
Precious-josiah-codes <45442741+Precious-josiah-codes@users.noreply.github.com>
2022-07-28 05:04:16 -0700

    made few changes

--- src/pages/testSignup.js
+++ src/pages/testSignup.js
@@ -1,4 +1,4 @@
-import { useContext, useReducer, useState } from "react";
+import { useContext, useEffect, useReducer, useState } from "react";
 import { ACTIONS } from "../state/actions/authActions";
 import { AuthContext } from "../state/contexts/AuthContext";
 import authReducer from "../state/reducers/AuthReducer";
@@ -7,13 +7,50 @@ const TestSignUp = () => {
     const [ email, setEmail ] = useState('')
     const [ password, setPassword ] = useState('')
     const  { state, signUp, authSuccess, signIn, updateState, error, createAccount } = useContext(AuthContext)
+    // const [ device, setDevice] = useState([
+    //     {deviceName: 'proxie kitchen', deviceLocation: 'office'},
+    //     {deviceName: 'airsyn room', deviceLocation: 'office'},
+    //     {deviceName: 'birsyn room', deviceLocation: 'office'},
+    // ])
 
+    const [allData,setAllData] = useState([
+        {deviceName: 'proxie kitchen', deviceLocation: 'office'},
+        {deviceName: 'airsyn room', deviceLocation: 'office'},
+        {deviceName: 'birsyn room', deviceLocation: 'office'},
+    ]);
+    
+    const [filteredData,setFilteredData] = useState(allData);
+
+    const handleSearch = (value) =>{
+        let result = [];
+        result = allData.filter((data) => {
+            return data.deviceName.includes(value)
+        })
+        setFilteredData(result);
+    }   
+
+    
+    useEffect(() => {
+        handleSearch(email)
+    }, [email])
+
+    // console.log(filterd)
+
+
+    // const device = [
+    //     {deviceName: 'proxie kitchen', deviceLocation: 'office'},
+    //     {deviceName: 'airsyn room', deviceLocation: 'office'},
+    //     {deviceName: 'birsyn room', deviceLocation: 'office'},
+    // ]
+
+    // const h = device.sort((a, b) => a.deviceName > b.deviceName ? 1 : -1);
+
+    // console.log(h, 'this is it')
 
     const handleSignUp = () => {
         console.log(email, password)
         console.log(state)
         signUp(email, password)
-
     }
 
     const handleSignIn = () => {
@@ -30,6 +67,12 @@ const TestSignUp = () => {
             <button className="bg-blue-900 text-white px-6 py-3" onClick={handleSignUp}>Sign up</button>
             <button className="bg-blue-900 text-white px-6 py-3" onClick={handleSignIn}>Sign in</button>
             <button className="bg-blue-900 text-white px-6 py-3" onClick={() => createAccount('preshy@gmail.com', '1292183182dfsd')}>Create account</button>
+            {
+                filteredData.sort((a, b) => a.deviceName > b.deviceName ? 1 : -1).map((d) => (
+                    <div key={d.deviceName}>{d.deviceName}</div>
+                ))
+                
+            }
         </div>
     );
 }
